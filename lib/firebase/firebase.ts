35d6b3e981d4898c3579e4c558629ca16ad74519
Devintel <devintelartbay@gmail.com>
2024-09-04 07:47:01 +0100

    init commit on clone branch

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
-import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
-  authDomain: 'anverso-f061e.firebaseapp.com',
-  projectId: 'anverso-f061e',
-  storageBucket: 'anverso-f061e.appspot.com',
-  messagingSenderId: '861220817704',
-  appId: '1:861220817704:web:56055a29b199e63e95de1d'
-}
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+	authDomain: 'ecommerce-8c0a1.firebaseapp.com',
+	projectId: 'ecommerce-8c0a1',
+	storageBucket: 'ecommerce-8c0a1.appspot.com',
+	messagingSenderId: '1008509629181',
+	appId: '1:1008509629181:web:28a26b92ca8dd359fe4494',
+};
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
+export const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 // Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app)
+export const auth = getAuth(app);
